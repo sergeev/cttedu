2085ed027b43d33e50b8bda54e472ab7bbf70ecf
sergeev <v.sergeev.m@gmail.com>
2021-09-04 14:57:07 +0300

    Init Spring boot cttEdu

---
+++ src/main/java/com/asro/ctt/MeridEducationApplication.java
@@ -0,0 +1,13 @@
+package com.asro.ctt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeridEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeridEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asro/ctt/MeridEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asro.ctt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeridEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
